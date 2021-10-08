67c9ba72b7a48bcdf135b1ae1b097c6f65bfc57e
frknn <frknstbs@gmail.com>
2021-10-08 22:50:19 +0300

    Add unit tests for Developer Create and Update Command Validators

---
+++ GameStore.Tests/GameStore.UnitTests/Application/Developers/Commands/CreateDeveloper/CreateDeveloperCommandValidatorTests.cs
@@ -0,0 +1,51 @@
+﻿using GameStore.Application.Developers.Commands.CreateDeveloper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GameStore.UnitTests.Application.Developers.Commands.CreateDeveloper
+{
+  public class CreateDeveloperCommandValidatorTests
+  {
+    [Fact]
+    public async Task Validator_WhenValidInputsAreGiven_ShouldNotReturnError()
+    {
+      CreateDeveloperCommand command = new()
+      {
+
+        Name = "category",
+        Country = "country"
+      };
+
+      CreateDeveloperCommandValidator validator = new();
+      var validationResult = await validator.ValidateAsync(command);
+
+      Assert.Empty(validationResult.Errors);
+    }
+
+    [Theory]
+    [InlineData("a", "a")]
+    [InlineData("", "")]
+    [InlineData(" ", " ")]
+    [InlineData("  ", "  ")]
+    [InlineData("   ", "   ")]
+    [InlineData("valid", "invalid country name that is longer than 20 characters")]
+    [InlineData("invalid developer name that is longer than 20 characters", "valid")]
+    public async Task Validator_WhenInvalidInputsAreGiven_ShouldNotReturnError(string name, string country)
+    {
+      CreateDeveloperCommand command = new()
+      {
+        Name = name,
+        Country = country
+      };
+
+      CreateDeveloperCommandValidator validator = new();
+      var validationResult = await validator.ValidateAsync(command);
+
+      Assert.NotEmpty(validationResult.Errors);
+    }
+  }
+}
---
+++ GameStore.Tests/GameStore.UnitTests/Application/Developers/Commands/UpdateDeveloper/UpdateDeveloperCommandValidatorTests.cs
@@ -0,0 +1,47 @@
+﻿using GameStore.Application.Developers.Commands.UpdateDeveloper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GameStore.UnitTests.Application.Developers.Commands.UpdateDeveloper
+{
+  public class UpdateDeveloperCommandValidatorTests
+  {
+    [Fact]
+    public async Task Validator_WhenValidInputsAreGiven_ShouldNotReturnError()
+    {
+      UpdateDeveloperCommand command = new()
+      {
+
+        Name = "category",
+        Country = "country"
+      };
+
+      UpdateDeveloperCommandValidator validator = new();
+      var validationResult = await validator.ValidateAsync(command);
+
+      Assert.Empty(validationResult.Errors);
+    }
+
+    [Theory]
+    [InlineData("a", "a")]
+    [InlineData("valid", "invalid country name that is longer than 20 characters")]
+    [InlineData("invalid developer name that is longer than 20 characters", "valid")]
+    public async Task Validator_WhenInvalidInputsAreGiven_ShouldNotReturnError(string name, string country)
+    {
+      UpdateDeveloperCommand command = new()
+      {
+        Name = name,
+        Country = country
+      };
+
+      UpdateDeveloperCommandValidator validator = new();
+      var validationResult = await validator.ValidateAsync(command);
+
+      Assert.NotEmpty(validationResult.Errors);
+    }
+  }
+}
